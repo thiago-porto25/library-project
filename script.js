b753f4cf78e5cf16f3f138331cfe9d42e0b327c7
thiago-porto25 <thiago_porto25@hotmail.com>
2021-04-11 12:26:20 -0300

    create css file and create function to add new book

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
+let myLibrary = []
+
 function Book(title, author, pages, read) {
-  this.title = title,
-  this.author = author,
-  this.pages = pages,
-  this.read = read,
-  this.info = () => {
+  (this.title = title),
+  (this.author = author),
+  (this.pages = pages),
+  (this.read = read),
+  (this.info = () => {
     return `${title} by ${author}, ${pages} pages, ${read}`
-  }
+  })
 }
 
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'not read yet')
+function takeInput() {
+title = prompt('What is the title of the book?', '')
+author = prompt('Who is the author of the book?', '')
+pages = prompt('How many pages does the book have?', '')
 
-console.log(theHobbit.info())
+}
+
+function addBookToLibrary(title, author, pages, read) {
+  const newBook = new Book(title, author, pages, read)
+  myLibrary.push(newBook)
+}
+
+const addBook = document.querySelector('#newBookButton') 
+addBook.addEventListener('click', takeInput)
