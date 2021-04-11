968756820dd032bd15c113b01a82ab1d816ee905
thiago-porto25 <thiago_porto25@hotmail.com>
2021-04-11 13:22:43 -0300

    add html tags and overall structuring of the page

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ function Book(title, author, pages, read) {
   (this.title = title),
   (this.author = author),
   (this.pages = pages),
-  (this.read = read),
-  (this.info = () => {
-    return `${title} by ${author}, ${pages} pages, ${read}`
-  })
+  (this.read = read)
+}
+
+function addBookToLibrary(title, author, pages, read) {
+  const newBook = new Book(title, author, pages, read)
+  myLibrary.push(newBook)
 }
 
 function takeInput() {
@@ -17,10 +19,6 @@ pages = prompt('How many pages does the book have?', '')
 
 }
 
-function addBookToLibrary(title, author, pages, read) {
-  const newBook = new Book(title, author, pages, read)
-  myLibrary.push(newBook)
-}
 
 const addBook = document.querySelector('#newBookButton') 
 addBook.addEventListener('click', takeInput)
