18819f346fd84d6049596b3c6488b61b6188e191
thiago-porto25 <thiago_porto25@hotmail.com>
2021-04-14 11:03:33 -0300

    add delete functionality but it is buggy asa he**

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 let myLibrary = []
 
-
 function Book(id, title, author, pages, read) {
-  (this.id = id),
-  (this.title = title),
-  (this.author = author),
-  (this.pages = pages),
-  (this.read = read)
+  ;(this.id = id),
+    (this.title = title),
+    (this.author = author),
+    (this.pages = pages),
+    (this.read = read)
 }
 
 
 const addBook = document.querySelector('#newBookButton')
 addBook.addEventListener('click', openModal)
-
 
 function openModal() {
   const modal = document.querySelector('#myModal')
@@ -22,8 +20,8 @@ function openModal() {
   modal.style.display = 'block'
 
   closeBtn.addEventListener('click', closeModal)
-  window.addEventListener('click', (event) => {
-    if(event.target == modal) {
+  window.addEventListener('click', event => {
+    if (event.target == modal) {
       modal.style.display = 'none'
     }
   })
@@ -32,46 +30,43 @@ function openModal() {
   submitBtn.addEventListener('click', takeInput)
 }
 
-
 function closeModal() {
   const modal = document.querySelector('#myModal')
   modal.style.display = 'none'
 }
 
-
 function takeInput() {
   const inputTitle = document.querySelector('#inputTitle')
-  const inputAuthor =  document.querySelector('#inputAuthor')
-  const inputPages =  document.querySelector('#inputPages')
-  const inputRead =  document.querySelector('#inputReadYes')
+  const inputAuthor = document.querySelector('#inputAuthor')
+  const inputPages = document.querySelector('#inputPages')
+  const inputRead = document.querySelector('#inputReadYes')
 
-  const id = myLibrary.length
+  const id = myLibrary.length // find a new way
   const title = inputTitle.value
   const author = inputAuthor.value
   const pages = inputPages.value
   let read = ''
 
-  if(document.querySelector('#inputReadYes') == false) {
+  if (document.querySelector('#inputReadYes') == false) {
     read = false
   } else {
     read = true
   }
 
   addBookToLibrary(id, title, author, pages, read)
-  
+
   inputTitle.value = ''
   inputAuthor.value = ''
   inputPages.value = ''
   inputRead.checked = false
 }
 
-
 function addBookToLibrary(id, title, author, pages, read) {
   const newBook = new Book(id, title, author, pages, read)
   myLibrary.push(newBook)
+  console.log(myLibrary)
   createCard(id)
 }
-
 
 function createCard(id) {
   const libContainer = document.querySelector('#libraryContainer')
@@ -79,6 +74,7 @@ function createCard(id) {
 
   let newCard = document.createElement('div')
   newCard.innerHTML = templateCard.innerHTML
+  newCard.setAttribute('data-delete', `${id}`)
 
   const title = newCard.querySelector('header')
   const author = newCard.querySelector('#author')
@@ -88,7 +84,20 @@ function createCard(id) {
   pages.textContent = `${myLibrary[id].pages} pages`
 
   libContainer.appendChild(newCard)
+
+  const deleteCardBtns = document.querySelectorAll(`#libraryContainer div[data-delete="${id}"] button`)
+  deleteCardBtns.forEach(button => button.addEventListener('click', (event) => {
+    if(event.target == button){
+      deleteId = id
+      console.log(deleteId)
+      myLibrary.splice(deleteId, 1)
+      const cardDeleted = document.querySelector(`[data-delete="${deleteId}"]`)
+      cardDeleted.remove()
+      console.log(myLibrary)
+      if(myLibrary.length == 1) myLibrary = []
+    }
+  }))
 }
 
-
-// change the Hobbit div so that it is hidden and not accessible but we can still copy it for the new divs.
+// if the queryselector works we can use the data-delete to delete the in the library. when the delete button is pressed
+// we select the div that was pressed
